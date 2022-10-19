05496658c91373f076c2d08a2d1123df97f73960
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-10-19 14:28:52 -0400

    push most recent changes

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,10 +1,34 @@
-// const menu = document.querySelector('.menu') as HTMLButtonElement;
-// const navList = document.querySelector('.nav-list') as HTMLUListElement;
-// const buttonsWrapper = document.querySelector(
-//   '.header-top__wrapper--buttons'
-// ) as HTMLDivElement;
-// let toggleBool: boolean = false;
-// menu.addEventListener('click', function (e) {
-//   toggleBool = !toggleBool;
-//   menu.setAttribute('aria-expanded', String(toggleBool));
-// });
+var menuButton = document.querySelector('.menu');
+var navbar = document.querySelector('.header-top__navbar');
+var tabItemWithPopup = Array.from(document.querySelectorAll('.tab__item[aria-haspopup="true"]'));
+var tabWithSubMenu = Array.from(document.querySelectorAll('.tab.has-submenu'));
+var subMenus = Array.from(document.querySelectorAll('sub-menu'));
+// toggle menu on click
+menuButton.addEventListener('click', function () {
+    var _a, _b;
+    navbar.classList.toggle('active');
+    if (navbar.classList.contains('active')) {
+        menuButton.setAttribute('aria-expanded', 'true');
+        (_a = menuButton === null || menuButton === void 0 ? void 0 : menuButton.firstElementChild) === null || _a === void 0 ? void 0 : _a.setAttribute('src', './develop/assets/images/icon-close.svg');
+    }
+    else {
+        menuButton.setAttribute('aria-expanded', 'false');
+        (_b = menuButton === null || menuButton === void 0 ? void 0 : menuButton.firstElementChild) === null || _b === void 0 ? void 0 : _b.setAttribute('src', './develop/assets/images/icon-hamburger.svg');
+    }
+});
+// show submenu on hover
+tabWithSubMenu.forEach(function (tab) {
+    tab.addEventListener('mouseover', function () {
+        var _a;
+        (_a = tab.firstElementChild) === null || _a === void 0 ? void 0 : _a.setAttribute('aria-expanded', 'true');
+    });
+    tab.addEventListener('mouseout', function () {
+        closeTab(tab);
+    });
+});
+function closeTab(tab) {
+    setTimeout(function () {
+        var _a;
+        (_a = tab.firstElementChild) === null || _a === void 0 ? void 0 : _a.setAttribute('aria-expanded', 'false');
+    }, 2000);
+}
--- develop/js/script.ts
+++ develop/js/script.ts
@@ -1,12 +1,47 @@
-// const menu = document.querySelector('.menu') as HTMLButtonElement;
-// const navList = document.querySelector('.nav-list') as HTMLUListElement;
-// const buttonsWrapper = document.querySelector(
-//   '.header-top__wrapper--buttons'
-// ) as HTMLDivElement;
-// let toggleBool: boolean = false;
+const menuButton = document.querySelector('.menu') as HTMLButtonElement;
+const navbar = document.querySelector('.header-top__navbar') as HTMLElement;
+const tabItemWithPopup = Array.from(
+  document.querySelectorAll('.tab__item[aria-haspopup="true"]')
+) as HTMLAnchorElement[];
+const tabWithSubMenu = Array.from(
+  document.querySelectorAll('.tab.has-submenu')
+) as HTMLLIElement[];
+const subMenus = Array.from(
+  document.querySelectorAll('sub-menu')
+) as HTMLUListElement[];
 
-// menu.addEventListener('click', function (e) {
-//   toggleBool = !toggleBool;
+// toggle menu on click
+menuButton.addEventListener('click', () => {
+  navbar.classList.toggle('active');
 
-//   menu.setAttribute('aria-expanded', String(toggleBool));
-// });
+  if (navbar.classList.contains('active')) {
+    menuButton.setAttribute('aria-expanded', 'true');
+    menuButton?.firstElementChild?.setAttribute(
+      'src',
+      './develop/assets/images/icon-close.svg'
+    );
+  } else {
+    menuButton.setAttribute('aria-expanded', 'false');
+    menuButton?.firstElementChild?.setAttribute(
+      'src',
+      './develop/assets/images/icon-hamburger.svg'
+    );
+  }
+});
+
+// show submenu on hover
+tabWithSubMenu.forEach((tab) => {
+  tab.addEventListener('mouseover', () => {
+    tab.firstElementChild?.setAttribute('aria-expanded', 'true');
+  });
+
+  tab.addEventListener('mouseout', () => {
+    closeTab(tab);
+  });
+});
+
+function closeTab(tab: HTMLLIElement): void {
+  setTimeout(() => {
+    tab.firstElementChild?.setAttribute('aria-expanded', 'false');
+  }, 2000);
+}
